7aa178bbb4de4c527e03e3fd55814e6b70b4f39e
jjobJunior <54251905+jjobJunior@users.noreply.github.com>
2024-04-30 17:09:36 -0300

    Navegação entre paginas com Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <h1>React Router</h1>
+      <NavBar/>
+      <Outlet />
+      {/* <Home />
+      <About/> */}
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './index.css'
+import "./index.css";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children:[]
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
