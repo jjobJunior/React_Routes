423fa96a9f500c1558dc29a2d68b78430fb386a6
jjobJunior <54251905+jjobJunior@users.noreply.github.com>
2024-05-01 11:32:35 -0300

    Substituindo Link por NavLink

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,18 @@
 import "./NavBar.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">Sobre</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/about">Sobre</Link> */}
+      <NavLink
+        to="/"
+        // className={({ isActive }) => (isActive ? "" : "")}
+      >
+        Home
+      </NavLink>
+      <NavLink to="/about">Sobre</NavLink>
     </nav>
   );
 };
